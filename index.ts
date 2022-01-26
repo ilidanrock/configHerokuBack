3ef9a20c88a1b165bf40038fc2646d263d882189
Allan Torres <allaneduardot@gmail.com>
2022-01-25 23:00:50 -0400

    cambio a typescript

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Response, Request } from 'express';
+const express =require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io =require('socket.io')(server,{cors:{origin: "*"}});
+ 
+
+app.get('/', (req:Request,res:Response)=>{
+    res.send('home');
+})
+
+server.listen(process.env.PORT || 3001, ()=>{
+    console.log("Server running...");
+})
+ 
